c506cfebb25f744d1a9f1fb401410911051086b9
various.authors <various.authors@gmail.com>
2019-12-14 23:16:34 -0800

    corecumber searches for and finds ores

--- src/main/java/com/arrogantgamer/cucurbits/block/CorecumberStemBlock.java
+++ src/main/java/com/arrogantgamer/cucurbits/block/CorecumberStemBlock.java
@@ -1,8 +1,12 @@
 package com.arrogantgamer.cucurbits.block;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 import javax.annotation.Nullable;
+
+import com.arrogantgamer.cucurbits.Cucurbits;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -13,10 +17,16 @@ import net.minecraft.block.StemBlock;
 import net.minecraft.block.StemGrownBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.Direction;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.storage.loot.LootParameters;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -49,8 +59,8 @@ public class CorecumberStemBlock extends StemBlock {
 		    Block block = soil.getBlock();
 		    if (canFruit(worldIn, fuelpos, blockpos) && (soil.canSustainPlant(worldIn, blockpos.down(), Direction.UP, this) || block == Blocks.FARMLAND
 			    || block == Blocks.DIRT || block == Blocks.COARSE_DIRT || block == Blocks.PODZOL || block == Blocks.GRASS_BLOCK)) {
-			consumeFuel(worldIn, fuelpos);
-			worldIn.setBlockState(blockpos, this.getFruit());
+			List<ItemStack> products = consumeFuel(worldIn, fuelpos);
+			worldIn.setBlockState(blockpos, this.getFruit(products));
 			worldIn.setBlockState(pos, this.crop.getAttachedStem().getDefaultState().with(HorizontalBlock.HORIZONTAL_FACING, direction));
 		    }
 		}
@@ -60,35 +70,73 @@ public class CorecumberStemBlock extends StemBlock {
 	}
     }
 
-    protected BlockState getFruit () {
+    protected BlockState getFruit (List<ItemStack> products) {
+	if (products.isEmpty()) return this.crop.getDefaultState();
+	
+	Cucurbits.LOGGER.debug("found: ");
+	
+	products.forEach((product) -> {
+	    Cucurbits.LOGGER.debug(product.getItem().getRegistryName());
+	});
+	// get the crop and fill it with the ore we found
+	
 	return this.crop.getDefaultState();
     }
     
-    protected void consumeFuel(World worldIn, BlockPos blockpos) {
-	worldIn.setBlockState(blockpos, Blocks.COBBLESTONE.getDefaultState());
+    protected List<ItemStack> consumeFuel(World worldIn, BlockPos blockpos) {
+	BlockState potentialOre = worldIn.getBlockState(blockpos);
+	Cucurbits.LOGGER.debug("-> consumeFuel");
+
+	if (isOre(potentialOre)) {
+		Cucurbits.LOGGER.debug("-> isOre");
+	    if (worldIn instanceof ServerWorld) {
+		Cucurbits.LOGGER.debug("-> isServer");
+
+                LootContext.Builder builder = (new LootContext.Builder((ServerWorld)worldIn)).withRandom(worldIn.rand).withParameter(LootParameters.POSITION, blockpos).withParameter(LootParameters.TOOL, ItemStack.EMPTY);
+		List<ItemStack> drops = potentialOre.getDrops(builder);
+		// TODO do this later once things are working otherwise we will destroy all the ore down there for testing :D
+		// worldIn.setBlockState(blockpos, Blocks.COBBLESTONE.getDefaultState());	
+		
+		return drops;
+	    }
+	}
+	
+	return Collections.emptyList();
     }
 
+    private ResourceLocation oreTagId = new ResourceLocation("forge", "ores");
+
+    
     @Nullable
     protected BlockPos findFuel(World worldIn, BlockPos blockpos, Random random) {
 	BlockPos fuelpos = null;
 
 	for (int i = 0; i < 4; ++i) {
-	    fuelpos = blockpos.add(random.nextInt(3) - 1, random.nextInt(5) - 5, random.nextInt(3) - 1);
+	    fuelpos = blockpos.add(random.nextInt(3) - 1, random.nextInt(blockpos.getY()) - blockpos.getY() - 1, random.nextInt(3) - 1);
+	    BlockState potentialOre = worldIn.getBlockState(fuelpos);
+
+	    Cucurbits.LOGGER.debug("fuel found: " + potentialOre.getBlock().getRegistryName());
 
 	    // check the drops for ore tag?
-	    if (worldIn.getBlockState(fuelpos).getMaterial() == Material.LAVA) {
+	    if (isOre(potentialOre)) {
+		Cucurbits.LOGGER.debug("success!");
+
 		break;
-	    }
+	    };
 
 	    fuelpos = null;
 	}
 
 	return fuelpos;
     }
+    
+    private boolean isOre(BlockState potentialOre) {
+	return BlockTags.getCollection().getOrCreate(oreTagId).contains(potentialOre.getBlock());
+    }
 
     protected boolean canFruit(World worldIn, @Nullable BlockPos fuelpos, BlockPos blockpos) {
 	// check that we did find fuel
-	return fuelpos != null && worldIn.getBlockState(blockpos).isAir(worldIn, blockpos) && worldIn.getBlockState(fuelpos).getMaterial() == Material.LAVA;
+	return fuelpos != null && worldIn.getBlockState(blockpos).isAir(worldIn, blockpos) && isOre(worldIn.getBlockState(fuelpos));
     }
 
     // I'm not sure that we need this...
