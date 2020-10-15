6062995ac58e50d4dc2d5552f1f0c605a7aee4a6
various.authors <various.authors@gmail.com>
2020-10-14 21:04:40 -0700

    Initial Commit

---
+++ src/main/java/com/arrogantgamer/cucurbits/Constants.java
@@ -0,0 +1,8 @@
+package com.arrogantgamer.cucurbits;
+
+public class Constants {
+	public static final String MODID = "cucurbits";
+	public static final String MODNAME = "Cucurbits";
+	public static final String VERSION = "0.0.1";
+	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12]";
+}
---
+++ src/main/java/com/arrogantgamer/cucurbits/Cucurbits.java
@@ -0,0 +1,29 @@
+package com.arrogantgamer.cucurbits;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Constants.MODID, name = Constants.MODNAME, version = Constants.VERSION, acceptedMinecraftVersions = Constants.ACCEPTED_MINECRAFT_VERSIONS)
+public class Cucurbits {
+    @Instance
+    public static Cucurbits instance;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+	System.out.println(Constants.MODID + ":preInit");
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+	System.out.println(Constants.MODID + ":init");
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+	System.out.println(Constants.MODID + ":postInit");
+    }
+}
