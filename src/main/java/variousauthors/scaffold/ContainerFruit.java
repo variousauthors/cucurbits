fbec1a223f7e7cc8637cf58054bb1ab5c9516c77
= <variousauthors@gmail.com>
2021-12-30 14:16:06 -0800

    Tidying up
     - remove a lot of null checking in the ContainerFruit default implementations

--- src/main/java/variousauthors/scaffold/ContainerFruit.java
+++ src/main/java/variousauthors/scaffold/ContainerFruit.java
@@ -11,88 +11,81 @@ import net.minecraft.world.World;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 import org.lwjgl.Sys;
+import scala.tools.cmd.gen.AnyValReps;
+
+import java.util.Optional;
 
 /* TODO got to generalize this, the default behaviour
 *   should be multi-slot (OR, have MultiSlotContainerFruit out there) */
 public interface ContainerFruit<TE extends TileEntity> {
     TE getTileEntity(IBlockAccess world, BlockPos pos);
 
-    default void getContents(NonNullList<ItemStack> drops, World worldIn, BlockPos pos) {
+    default Optional<IItemHandler> getItemHandler (World worldIn, BlockPos pos) {
         TE tile = getTileEntity(worldIn, pos);
 
-        if (tile == null) return;
+        if (tile == null) return Optional.empty();
 
         IItemHandler itemHandler = tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.NORTH);
 
-        if (itemHandler == null) return;
+        return Optional.ofNullable(itemHandler);
+    }
 
-        ItemStack stack = itemHandler.getStackInSlot(0);
+    default void getContents(NonNullList<ItemStack> drops, World worldIn, BlockPos pos) {
+        getItemHandler(worldIn, pos).ifPresent(itemHandler -> {
+            ItemStack stack = itemHandler.getStackInSlot(0);
 
-        if (!stack.isEmpty()) {
-            drops.add((stack));
-        }
+            if (!stack.isEmpty()) {
+                drops.add((stack));
+            }
+        });
     }
 
     default NonNullList<ItemStack> insertContents(NonNullList<ItemStack> drops, World worldIn, BlockPos pos) {
-        TE tile = getTileEntity(worldIn, pos);
-
-        if (tile == null) return drops;
-
-        IItemHandler itemHandler = tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.NORTH);
-
-        if (itemHandler == null) return drops;
-
         NonNullList<ItemStack> remainder = NonNullList.create();
 
-        for (ItemStack drop : drops) {
-            ItemStack result = itemHandler.insertItem(0, drop, false);
+        return getItemHandler(worldIn, pos)
+                .map(itemHandler -> {
+                    for (ItemStack drop : drops) {
+                        ItemStack result = itemHandler.insertItem(0, drop, false);
 
-            if (!result.isEmpty()) {
-                remainder.add(result);
-            }
-        }
+                        if (!result.isEmpty()) {
+                            remainder.add(result);
+                        }
+                    }
 
-        return remainder;
+                    return remainder;
+                })
+                .orElse(remainder);
     }
 
     default void extractContents(NonNullList<ItemStack> drops, World worldIn, BlockPos pos, int amount) {
-        TE tile = getTileEntity(worldIn, pos);
+        getItemHandler(worldIn, pos).ifPresent(itemHandler -> {
+            ItemStack stack = itemHandler.extractItem(0, amount, false);
 
-        if (tile == null) return;
+            if (!stack.isEmpty()) {
+                drops.add((stack));
+            }
 
-        IItemHandler itemHandler = tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.NORTH);
+            ItemStack remainder = itemHandler.getStackInSlot(0);
 
-        if (itemHandler == null) return;
-
-        ItemStack stack = itemHandler.extractItem(0, amount, false);
-
-        if (!stack.isEmpty()) {
-            drops.add((stack));
-        }
-
-        ItemStack remainder = itemHandler.getStackInSlot(0);
-
-        if (remainder.isEmpty()) {
-            // remove the fruit
-            worldIn.removeTileEntity(pos);
-            worldIn.setBlockState(pos, Blocks.AIR.getDefaultState());
-        }
+            if (remainder.isEmpty()) {
+                // remove the fruit
+                worldIn.removeTileEntity(pos);
+                worldIn.setBlockState(pos, Blocks.AIR.getDefaultState());
+            }
+        });
     }
 
     /* by default there is just one slot, so this literally just checks if that
     * slot is full. */
     default boolean isFull (World worldIn, BlockPos pos) {
-        TE tile = getTileEntity(worldIn, pos);
+        return getItemHandler(worldIn, pos)
+                .map(itemHandler -> {
+                    ItemStack stack = itemHandler.getStackInSlot(0);
+                    System.out.println("stack: " + stack.getCount() + "/" + itemHandler.getSlotLimit(0));
 
-        if (tile == null) return false;
-
-        IItemHandler itemHandler = tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.NORTH);
-
-        if (itemHandler == null) return false;
-
-        ItemStack stack = itemHandler.getStackInSlot(0);
-        System.out.println("stack: " + stack.getCount() + "/" + itemHandler.getSlotLimit(0));
-
-        return !(stack.getCount() < itemHandler.getSlotLimit(0));
+                    return !(stack.getCount() < itemHandler.getSlotLimit(0));
+                })
+                .orElse(false);
     }
 }
